0a454a634acb376f4fce6c0ed077798b706bba16
Anton <lascivolin@gmail.com>
2017-05-30 21:00:06 +0300

    custom get request

--- server/index.js
+++ server/index.js
@@ -8,7 +8,14 @@ app.get('/api/test', function (req, res) {
 
 app.get('/api/items', function (req, res) {
   const fileName = './server/products-mock.json';
-  const data =  fs.readFileSync(fileName, 'utf8');
+  let data =  fs.readFileSync(fileName, 'utf8');
+
+  if (req.query.offset && req.query.count) {
+    const parsedData = JSON.parse(data);
+    parsedData.items = parsedData.items.splice(req.query.offset, req.query.count);
+    data = JSON.stringify(parsedData);
+  }
+
   setTimeout(() => (res.send(data)), 500);
 })
 
