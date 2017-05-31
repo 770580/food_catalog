4b4de01898c7f6dc446a25a59fa9f4b3b4c8345d
Anton <lascivolin@gmail.com>
2017-05-31 11:58:29 +0300

    fix api-server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
+let data = require('./products-mock.json');
 
 app.get('/api/test', function (req, res) {
   res.send('API server works!');
 })
 
 app.get('/api/items', function (req, res) {
-  const fileName = './server/products-mock.json';
-  let data =  fs.readFileSync(fileName, 'utf8');
-
+  let slicedData = {};
   if (req.query.offset && req.query.count) {
-    const parsedData = JSON.parse(data);
-    parsedData.items = parsedData.items.splice(req.query.offset, req.query.count);
-    data = JSON.stringify(parsedData);
+    slicedData.items = data.items.slice(req.query.offset, Number(req.query.offset) + Number(req.query.count));
+  } else {
+    slicedData = data;
   }
 
-  setTimeout(() => (res.send(data)), 500);
+  setTimeout(() => (res.send(JSON.stringify(slicedData))), 500);
 })
 
 app.listen(3003, function () {
